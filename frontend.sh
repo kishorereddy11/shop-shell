8caf7babc5c8edc86f4b719fe24e2b1714aa6d4e
kishor <kishorebsc12@gmail.com>
2025-02-17 13:34:35 +0530

    example

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,10 @@ systemctl enable nginx
 
 rm -rf /usr/share/nginx/html/*
 
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
+#curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+
+
 
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
